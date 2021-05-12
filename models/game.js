2827b04a0f0d43e9de4c347ef99baa5f3198cd91
mtorcellini <matt.torcellini@gmail.com>
2021-05-12 16:41:05 -0400

    create Game model

--- models/game.js
+++ models/game.js
@@ -1,3 +1,22 @@
-const {model, DataTypes} = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+
+const Game = sequelize.define('Game', {
+  id : {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  winner : {
+    type: DataTypes.STRING,
+  },
+  loser: {
+    type: DataTypes.STRING,
+  },
+  tie: {
+    type: DataTypes.BOOLEAN,
+  }
+})
+
+module.exports = Game;
