2b78c96988ca6e2fabc8503aa860820d6d4b27a6
mtorcellini <matt.torcellini@gmail.com>
2021-05-12 17:04:16 -0400

    connect routes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // express
 const express = require('express');
 const sequelize = require('./config/connection.js');
+const routes = require('./routes');
 
 // set up handlebars engine
 const exphbs = require('express-handlebars');
@@ -11,6 +12,9 @@ const app = express();
 app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use(routes);
+
 app.get('/', (req, res) => {
   // res.render('home');
   res.render('game');
