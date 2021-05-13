1cc9560643676d1a3491a051e6f51fffef95df41
mtorcellini <matt.torcellini@gmail.com>
2021-05-13 18:09:47 -0400

    add user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const {DataTypes} = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    notNull: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    notNull: true,
+  }
+})
