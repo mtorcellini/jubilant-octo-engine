8288b38ce55e4d73a830099586f592415b98a1c9
mtorcellini <matt.torcellini@gmail.com>
2021-05-13 20:18:19 -0400

    server side login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,23 @@ router.post('/', async (req, res) => {
   } catch (err) {
     res.json(err);
   }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    console.log(req.body);
+    const userData = await User.findOne({where: 
+      {username: req.body.username}
+    });
+
+    if (!userData) {
+      console.log('no user');
+      res.status(400).json("Incorrect username or password");
+    }
+
+  } catch (err) {
+    res.json(err);
+  }
 })
 
 
