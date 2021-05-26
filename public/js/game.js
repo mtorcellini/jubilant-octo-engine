de136899de3c1cc0bd716e1796ad22e29acdf199
mtorcellini <matt.torcellini@gmail.com>
2021-05-25 23:39:56 -0400

    beautify game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,48 +1,35 @@
 $(document).ready(() => {
 
-  let player;
-
-  // initialize empty game state
-  let gameState = {
-    player_one : [],
-    player_two : []
+  // GLOBALS //
+  let PLAYER;
+  let GAMEID;
+  let GAMESTATE = {
+    player_one: [],
+    player_two: []
   };
-  let gameId;
 
-  // hide gameboard initially
-  //$('#gameboard').hide();
 
-  // hide idText initially
-  //$('#idText').hide();
-
-  // function for making a new Game, server sets a session var with current game ID
+  // MAKE NEW GAME
   const makeNewGame = () => {
-    player = "player_one";
+    PLAYER = "player_one";
 
     fetch('/api/game', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    .then(response => response.json())
-    .then(response => {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
 
-      // set gameId according to response
-      gameId = response.id;
+        GAMEID = response.id;
 
-      $('#player-one').text(response.player_one);
-
-      // show id text
-      $('#gameIdSpan').text(gameId);
-      $('#idText').show();
-
-      // show gameboard
-      $('#gameboard').show();
-
-      // hide startgame button
-      $('#makeNew').hide();
-    })
+        $('#player-one').text(response.player_one);
+        $('#gameIdSpan').text(GAMEID);
+        $('#idText').show();
+        $('#gameboard').show();
+        $('#makeNew').hide();
+      })
   }
 
 
@@ -51,21 +38,15 @@ $(document).ready(() => {
     fetch(`/api/game`)
       .then(response => response.json())
       .then(response => {
-
-        console.log(response);
-
         const gameData = response.gameData;
+        // WHO AM I
+        PLAYER = response.player;
 
         // MARK BOARD
         if (gameData) {
           markSquares(gameData.state);
         }
 
-        // WHO AM I
-        console.log('player: ', response.player);
-        player = response.player;
-
-        // display game ID
         $('#gameIdSpan').text(gameData.id);
         $('#idText').show();
 
@@ -86,72 +67,67 @@ $(document).ready(() => {
         data.player_one.forEach(xmark => {
           const xCoord = xmark[0];
           const yCoord = xmark[1];
-          $(`.row${yCoord} .col${xCoord}`).css({ background: "darkblue", color: "white" });
+          $(`.row${yCoord} .col${xCoord}`).css({
+            background: "darkblue",
+            color: "white"
+          });
         });
       }
       if (data.player_two) {
         data.player_two.forEach(omark => {
           const xCoord = omark[0];
           const yCoord = omark[1];
-          $(`.row${yCoord} .col${xCoord}`).css({ background: "purple", color: "white" });
+          $(`.row${yCoord} .col${xCoord}`).css({
+            background: "purple",
+            color: "white"
+          });
         })
       }
     }
   }
 
-  // function for submitting data of a new move 
+  // SEND MOVE REQUEST
   const makeMove = (gameState) => {
-
-    // send gameState to server
     fetch(`/api/game`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(gameState)
-    })
-
-      // get newest data from server and mark squares
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(gameState)
+      })
       .then(() => {
         getGameData();
       })
   }
 
-  
-  // click listener on gameboard
+
+  // CLICK LISTENER ON GAMEBOARD
   $("#gameboard").on("click", async (event) => {
     const xcoord = parseInt(event.target.getAttribute("data"));
     const ycoord = parseInt(event.target.parentElement.getAttribute("data"));
     const coords = [xcoord, ycoord];
 
-    // update local game state
-    gameState[player].push(coords);
+    // UPDATE LOCAL GAMESTATE
+    GAMESTATE[PLAYER].push(coords);
 
-    // send gamestate to server
-    makeMove(gameState);
+    // SEND PUT REQUEST
+    makeMove(GAMESTATE);
   })
 
+  // DELETE GAME ON LEAVE-BUTTON
   $('#leaveGame').on('click', () => {
-
-    // delete current game
     fetch('/api/game', {
-      method: 'DELETE'
-    })
+        method: 'DELETE'
+      })
       .then(() => {
         location.href = "/"
       })
   })
 
+  $('#gameboard').hide();
+  $('#idText').hide();
   $("#test").on('click', getGameData);
   $('#makeNew').on('click', makeNewGame);
-
-
-
-// run this function to get a game if there is a session variable set
-getGameData();
-
-const timer = setInterval(getGameData, 5000);
-
-
+  const timer = setInterval(getGameData, 5000);
 
 })
